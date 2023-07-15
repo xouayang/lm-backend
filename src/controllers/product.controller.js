edbc08ba33dd0abffdac410dfe1c4ad2caa221ae
Xouayang <xouayang2000@gmail.com>
2023-07-15 21:14:20 +0700

    fix product api

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,6 +14,7 @@ exports.create = async (req, res) => {
         return res.status(404).json({message:error.message})
       })  
     } catch (error) {
+      console.log(error)
      return res.status(500).json({message:error.message})   
     }
 }
