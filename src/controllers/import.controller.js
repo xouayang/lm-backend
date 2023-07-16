dfc051a39d47797da8331b3db0b79432dd6da547
Xouayang <xouayang2000@gmail.com>
2023-07-16 22:45:13 +0700

    fix import api

--- src/controllers/import.controller.js
+++ src/controllers/import.controller.js
@@ -1,17 +1,19 @@
 const imports = require("../model/import.model");
 const importDetails = require("../model/importDetails.model");
-const product = require('../model/prduct.model')
+const product = require('../model/prduct.model');
+const sequelize = require('../configs/db');
+const {QueryTypes} = require('sequelize');
 exports.create_import = async (req, res) => {
   try {
+    let result=[]
+    const employee_id = req.payload.id
     const {
       order_id,
-      employee_id,
       import_date,
       import_quaty,
       import_total_kip,
     } = req.body;
     const item = req.body.item;
-
     for (let i = 0; i < item.length; i++) {
       await product.increment("quatity", {
         by: item[i].quatity,
@@ -29,16 +31,36 @@ exports.create_import = async (req, res) => {
         })
         .then(async (data) => {
           await importDetails.create({
-            order_id: data.id,
+            import_id: data.id,
             import_details_date: item[i].import_details_date,
             pro_id: item[i].pro_id,
             import_qty: item[i].import_qty,
             total_price: item[i].total_price,
-          });
+          }).then((data) => {
+            result.push(data)
+          }).catch((error) => {
+            console.log({message:error.message})
+          })
         });
     }
+    return res.status(200).json(result)
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 // get history of imports
+exports.get_history = async (req, res) => {
+    try {
+     const sql = `
+       select * from imports 
+     `
+    const data = await sequelize.query(sql,{type:QueryTypes.SELECT})    
+    if(data.length > 0) {
+        return res.status(200).json(data);
+    } else {
+        return res.status(200).json("Error");
+    }
+    } catch (error) {
+     return res.status(500).json({message:error.message})   
+    }
+}
--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,50 +1,55 @@
-const order = require('../model/order.model');
-const orderDetails = require('../model/orderDetails.model');
-const sequelize = require('../configs/db');
-const {QueryTypes} = require('sequelize');
-const {billNumber} = require('../helpers/randonBill')
-// create data 
+const order = require("../model/order.model");
+const orderDetails = require("../model/orderDetails.model");
+const sequelize = require("../configs/db");
+const { QueryTypes } = require("sequelize");
+const { billNumber } = require("../helpers/randonBill");
+// create data
 exports.create = async (req, res) => {
-    const bill = billNumber()
-    try {
-        let message = [] ;
-        const employee_id = req.payload.id
-        const bill_number = bill
-        const {order_qty,supllier_id,order_date,total_price} = req.body
-        const item = req.body.item;
-        for (let i = 0; i < item.length; i++) {
-           await order.create({
-            employee_id:employee_id,
-            order_qty:order_qty,
-            supllier_id:supllier_id,
-            order_date:order_date,
-            total_price:total_price,
-            bill_number:bill_number
-           }).then(async (data) =>{
-            await orderDetails.create({
-            order_id:data.id,
-            bill_number:data.bill_number,
-            product_id:item[i].product_id,
-            order_qty:item[i].order_qty,
-            total_price:item[i].total_price,
-            status:item[i].status,
-            order_details_date:item[i].order_details_date,
-            }).then((data) => {
-             message.push(data)
+  const bill = billNumber();
+  try {
+    let message = [];
+    const employee_id = req.payload.id;
+    const bill_number = bill;
+    const { order_qty, supllier_id, order_date, total_price } = req.body;
+    const item = req.body.item;
+    for (let i = 0; i < item.length; i++) {
+      await order
+        .create({
+          employee_id: employee_id,
+          order_qty: order_qty,
+          supllier_id: supllier_id,
+          order_date: order_date,
+          total_price: total_price,
+          bill_number: bill_number,
+        })
+        .then(async (data) => {
+          await orderDetails
+            .create({
+              order_id: data.id,
+              bill_number: data.bill_number,
+              product_id: item[i].product_id,
+              order_qty: item[i].order_qty,
+              total_price: item[i].total_price,
+              status: item[i].status,
+              order_details_date: item[i].order_details_date,
             })
-           }).catch((error) => {
-            console.log(error)
-           }) 
-        } 
-        return res.status(200).json(message) 
-    } catch (error) {
-     return res.status(500).json({message:error.message})   
+            .then((data) => {
+              message.push(data);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-}
+    return res.status(200).json(message);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 // get all order
-exports.get_all_order = async (req, res) =>{
- try {
-    const {id} = req.params;
+exports.get_all_order = async (req, res) => {
+  try {
+    const { id } = req.params;
     const sql = `
      select DISTINCT ord.id,ordt.product_id,ordt.bill_number,pdt.name as type_name,ut.name as unit_name
      ,pt.name, ord.order_qty,ord.order_date,ordt.order_qty as qty_small,ordt.total_price as order_details_price,
@@ -54,14 +59,14 @@ exports.get_all_order = async (req, res) =>{
      inner join product_types pdt on pt.product_type_id = pdt.id
      inner join units ut on pt.unit_id = ut.id
      where ordt.bill_number = '${id}'
-    `
-    const data = await sequelize.query(sql,{type:QueryTypes.SELECT})
-    if(data.length > 0) {
-        return res.status(200).json(data)
+    `;
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    if (data.length > 0) {
+      return res.status(200).json(data);
     } else {
-        return res.status(200).json(data)
+      return res.status(200).json(data);
     }
- } catch (error) {
-  return res.status(500).json({message:error.message})  
- }
-}
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
