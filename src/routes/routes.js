7ff18445189efc7235febb0926548a7028c54553
Xouayang <xouayang2000@gmail.com>
2023-07-16 13:14:40 +0700

    app exchange api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,10 @@ const routes = require("express").Router();
 const employee = require("./employee.routes");
 const customer = require("./customer.routes");
 const supplier = require("./supplier.routes");
-const ProductType = require('./productType.routes')
-const Unit = require('./unit.routes')
-const Product = require('./product.routes')
+const ProductType = require('./productType.routes');
+const Unit = require('./unit.routes');
+const Product = require('./product.routes');
+const Exchange = require('./exchange.routes');
 //use routes
 routes.use("/employee", employee);
 routes.use("/customer", customer);
@@ -12,4 +13,5 @@ routes.use("/supplier", supplier);
 routes.use("/productType", ProductType);
 routes.use("/Unit", Unit);
 routes.use("/Product", Product);
+routes.use("/Exchange", Exchange);
 module.exports = routes;
