4298c2ee692f16442eeb836f3a30c4672eb6724c
Xouayang <xouayang2000@gmail.com>
2023-07-21 13:15:16 +0700

    add top sale api

--- src/controllers/reports/all_reports.js
+++ src/controllers/reports/all_reports.js
@@ -1,6 +1,7 @@
 const customer = require("../../model/customer.model");
 const employee = require("../../model/employee.model");
-const product = require('../../model/prduct.model')
+const Product = require('../../model/prduct.model')
+const SaleDetail = require('../../model/sale_detail.model')
 const sequelize = require("../../configs/db");
 const { QueryTypes,Op } = require("sequelize");
 // get all
@@ -69,5 +70,21 @@ exports.product_finished = async (req, res) => {
     })
   } catch (error) {
   return res.status(500).json({message:error.message})  
+  }
+}
+// get top sale product
+exports.top_sale_product = async (req, res) => {
+  try {
+    const sql = `
+     select pdt.name as product_type_name,un.name as unit_name, pd.name,sdt.Sale_qty  from products pd 
+     inner join sale_details sdt on pd.id = sdt.Pro_id
+     inner join product_types pdt on pd.product_type_id = pdt.id
+     inner join units un on pd.unit_id = un.id
+     order by sdt.Sale_qty DESC LIMIT 10
+    `
+   const data = await sequelize.query(sql,{type:QueryTypes.SELECT}) 
+   return res.status(200).json(data)
+  } catch (error) {
+    return res.status(500).json({message:error.message})
   }
 }
--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -4,4 +4,5 @@ routes.get('/customer', controller.get_customer);
 routes.get('/employee', controller.get_employee);
 routes.get('/product', controller.get_all_product);
 routes.get('/product-finished', controller.product_finished);
+routes.get('/top-sale', controller.top_sale_product);
 module.exports = routes;
