f379ad58acc68b103d06f897890f801703956bce
Xouayang <xouayang2000@gmail.com>
2023-07-31 21:31:09 +0700

    fix all reports

--- src/controllers/reports/all_reports.js
+++ src/controllers/reports/all_reports.js
@@ -1,8 +1,8 @@
 const customer = require("../../model/customer.model");
 const employee = require("../../model/employee.model");
-const SaleDetail = require('../../model/sale_detail.model');
+const SaleDetail = require("../../model/sale_detail.model");
 const sequelize = require("../../configs/db");
-const { QueryTypes,Op } = require("sequelize");
+const { QueryTypes, Op } = require("sequelize");
 // get all
 exports.get_customer = async (req, res) => {
   try {
@@ -45,32 +45,35 @@ exports.get_all_product = async (req, res) => {
         from products pd 
        inner join product_types pdt on pd.product_type_id = pdt.id
     `;
-    const data = await sequelize.query(sql,{type:QueryTypes.SELECT});
-    if(data.length > 0) {
-      return res.status(200).json(data)
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    if (data.length > 0) {
+      return res.status(200).json(data);
     } else {
-      return res.status(200).json(data)
+      return res.status(200).json(data);
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
-// get product is on 
+// get product is on
 exports.product_finished = async (req, res) => {
   try {
-    await product.findAll({
-      where:{
-        quatity:{[Op.lte]: 10} 
-      }
-    }).then((data) => {
-      return res.status(200).json(data)
-    }).catch((error) => {
-      return res.status(200).json({message:error.message})
-    })
+    await product
+      .findAll({
+        where: {
+          quatity: { [Op.lte]: 10 },
+        },
+      })
+      .then((data) => {
+        return res.status(200).json(data);
+      })
+      .catch((error) => {
+        return res.status(200).json({ message: error.message });
+      });
   } catch (error) {
-  return res.status(500).json({message:error.message})  
+    return res.status(500).json({ message: error.message });
   }
-}
+};
 // get top sale product
 exports.top_sale_product = async (req, res) => {
   try {
@@ -80,35 +83,36 @@ exports.top_sale_product = async (req, res) => {
      inner join product_types pdt on pd.product_type_id = pdt.id
      inner join units un on pd.unit_id = un.id
      order by sdt.Sale_qty DESC LIMIT 10
-    `
-   const data = await sequelize.query(sql,{type:QueryTypes.SELECT}) 
-   return res.status(200).json(data)
+    `;
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({message:error.message})
+    return res.status(500).json({ message: error.message });
   }
-}
-// income 
+};
+// income
 exports.income = async (req, res) => {
   try {
-    await SaleDetail.findAll().then((data) => {
-      return res.status(200).json(data)
-    }).catch((error) => {
-      return res.status(200).json({message:error.message})
-    })
+    const sql = `
+    select DISTINCT  pd.name as product_name,sd.Sale_qty  from products pd 
+    inner join sale_details sd 
+   `;
+   const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+   return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({message:error.message})
+    return res.status(500).json({ message: error.message });
   }
-}
+};
 // outcome
 exports.outcome = async (req, res) => {
   try {
     const sql = `
      select DISTINCT pd.id, pd.name as product_name,imd.import_qty,imd.import_details_date,imd.total_price from products pd 
      inner join import_details imd on pd.id = imd.Pro_id
-    `
-    const data = await sequelize.query(sql,{type:QueryTypes.SELECT}) 
-    return res.status(200).json(data)
+    `;
+    const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({message:error.message})
+    return res.status(500).json({ message: error.message });
   }
-}
+};
